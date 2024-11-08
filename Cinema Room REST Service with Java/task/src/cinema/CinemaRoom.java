507ffece6842c3c2be9a808e55991abe45efce7b
ranudero <landerverbruggh@gmail.com>
2024-11-08 17:21:32 +0100

    feat : add features to purchaseSeat in CinemaRoom

--- Cinema Room REST Service with Java/task/src/cinema/CinemaRoom.java
+++ Cinema Room REST Service with Java/task/src/cinema/CinemaRoom.java
@@ -42,4 +42,28 @@ public class CinemaRoom {
     public void setSeats(List<Seat> seats) {
         this.seats = seats;
     }
+
+    public Seat purchaseSeat(int row, int column) {
+        for (Seat seat : seats) {
+            if (seat.getRow() == row && seat.getColumn() == column) {
+
+                seat.setPurchased(true);
+                return seat;
+            }
+        }
+        return null;
+    }
+
+    public boolean isValidSeat(int row, int column) {
+        return row >= 1 && row <= rows && column >= 1 && column <= columns;
+    }
+
+    public boolean isSeatPurchased(int row, int column) {
+        for (Seat seat : seats) {
+            if (seat.getRow() == row && seat.getColumn() == column) {
+                return seat.isPurchased();
+            }
+        }
+        return false;
+    }
 }
