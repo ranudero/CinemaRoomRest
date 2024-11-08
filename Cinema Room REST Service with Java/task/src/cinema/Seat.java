78a8417b95c0c25125ec9843cc5f6fde3970f38e
ranudero <landerverbruggh@gmail.com>
2024-11-08 17:21:52 +0100

    chore : ignore field purchased from JSON format

--- Cinema Room REST Service with Java/task/src/cinema/Seat.java
+++ Cinema Room REST Service with Java/task/src/cinema/Seat.java
@@ -1,16 +1,25 @@
 package cinema;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class Seat {
+
     private int row;
     private int column;
     private int price;
     final private int priceFront = 10;
     final private int priceBack = 8;
+    @JsonIgnore
+    private boolean purchased;
+
+    public Seat(){
+        //Default constructor
+    }
 
     public Seat(int row, int column) {
         this.row = row;
         this.column = column;
         this.price = row <= 4 ? priceFront : priceBack;
+        this.purchased = purchased;
     }
 
     public int getRow() {
@@ -35,5 +44,13 @@ public class Seat {
 
     public void setPrice(int price) {
         this.price = price;
+    }
+
+    public boolean isPurchased() {
+        return purchased;
+    }
+
+    public void setPurchased(boolean purchased) {
+        this.purchased = purchased;
     }
 }
