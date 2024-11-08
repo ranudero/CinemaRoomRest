fb573f18f3596a658813522a3f47a7d73407c47b
ranudero <landerverbruggh@gmail.com>
2024-11-08 17:22:35 +0100

    feat : add Postmapping to CinemaController so a seat can be purchased. Include exceptionhandling.

--- Cinema Room REST Service with Java/task/src/cinema/CinemaController.java
+++ Cinema Room REST Service with Java/task/src/cinema/CinemaController.java
@@ -1,15 +1,39 @@
 package cinema;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/seats")
+@RequestMapping()
 public class CinemaController {
     private final CinemaRoom cinemaRoom = new CinemaRoom(9, 9);
 
-    @RequestMapping()
+    @RequestMapping("/seats")
     public CinemaRoom getAllSeats() {
         return cinemaRoom;
+    }
+
+    @PostMapping("/purchase")
+    public ResponseEntity<Object> purchaseSeat(@RequestBody Seat seatRequest) {
+        if (!cinemaRoom.isValidSeat(seatRequest.getRow(), seatRequest.getColumn())) {
+            throw new IllegalArgumentException("The number of a row or a column is out of bounds!");
+        } else if (cinemaRoom.isSeatPurchased(seatRequest.getRow(), seatRequest.getColumn())) {
+            throw new IllegalStateException("The ticket has been already purchased!");
+        } else {
+            return ResponseEntity.ok(cinemaRoom.purchaseSeat(seatRequest.getRow(), seatRequest.getColumn()));
+        }
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.badRequest().body("{\"error\": \"" + ex.getMessage() + "\"}");
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleIllegalStateException(IllegalStateException ex) {
+        return ResponseEntity.badRequest().body("{\"error\": \"" + ex.getMessage() + "\"}");
     }
 }
